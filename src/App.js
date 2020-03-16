6f8ada58ce50b8d538919899d334e691f5d1371b
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-16 17:24:41 +0000

    commit 16.003.20

--- src/App.js
+++ src/App.js
@@ -1,77 +1,137 @@
 import React, { Component } from 'react'
-import './App.css';
-import Timer from './components/timer.js';
+import "./App.css";
+import Timer from './components/Timer.js';
 
 
 class App extends Component {
 
   state = {
     counter1: 0,
-    counter2: 0
+    counter2: 0,
+    currentTeamName1: '',
+    updatedTeamName1: [],
+    currentTeamName2: '',
+    updatedTeamName2: [],
   }
-
+// Points and Penalty fuctions for team 1
   try1 = () => {
     this.setState({
       counter1: this.state.counter1 + 5
     })
   }
-
   conversion1 = () => {
     this.setState({
       counter1: this.state.counter1 + 2
     })
   }
-
   dropGoal1 = () => {
     this.setState({
       counter1: this.state.counter1 + 3
     })
   }
-
   penalty1 = () => {
     this.setState({
       counter1: this.state.counter1 - 3
     })
   }
+
+  // Points and Penalty fuctions for team 2
   try2 = () => {
     this.setState({
       counter2: this.state.counter2 + 5
     })
   }
-
   conversion2 = () => {
     this.setState({
       counter2: this.state.counter2 + 2
     })
   }
-
   dropGoal2 = () => {
     this.setState({
       counter2: this.state.counter2 + 3
     })
   }
-
   penalty2 = () => {
     this.setState({
       counter2: this.state.counter2 - 3
     })
   }
 
+// Changing team names for teams 1 & 2
+  onClickTeamName1 = () => {
+    this.setState({
+      updatedTeamName1: [this.state.currentTeamName1]
+    })
+  }
+  onClickTeamName2 = () => {
+    this.setState({
+      updatedTeamName2: [this.state.currentTeamName2]
+    })
+  }
+  getValuesTeamName1 = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      currentTeamName1: e.target.value
+    })
+  }
+  getValuesTeamName2 = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      currentTeamName2: e.target.value
+    })
+  }
+  handleChangeTeamName1 = (e) => {
+    e.preventDefault();
+  }
+  handleChangeTeamName2 = (e) => {
+    e.preventDefault();
+  }
+
   render() {
+    const teamNames1 = this.state.updatedTeamName1.map((text, index) => {
+      return <h1>{text}</h1>
+    })
+
+    const teamNames2 = this.state.updatedTeamName2.map((text, index) => {
+      return <h1>{text}</h1>
+    })
+
     return (
       <div>
         <div className="container">
+          {/* Timer Function section*/}
           <div className="timeDisplay">
             <Timer time={Timer} text={"Time"} />
           </div>
+
+          {/* Score Display */}
           <div className="Scores">
-            <div className="score1">
-              <p>Score {this.state.counter1}</p>
-            </div>
-            <div className="score2">
-              <p>Score {this.state.counter2}</p>
-            </div>
+            <p>Score {this.state.counter1}</p>
+            <p>Score {this.state.counter2}</p>
           </div>
+
+          {/* Team Name input */}
+          <div className="teamNames">
+            {/* Team 1 change name input section */}
+            <label>
+              <input type="text" value={this.state.value} onChange={this.getValuesTeamName1} />
+              <button onClick={this.onClickTeamName1}>Change Team Name 1</button>
+              {teamNames1}
+              {/*TeamSheet shows points next to each player on what they have scored */}
+              <button onClick={this.props.closePopup}>Close</button>
+            </label>
+
+              {/* Team 2 change name input section */}
+            <label>
+              <input type="text" value={this.state.value} onChange={this.getValuesTeamName2} />
+              <button onClick={this.onClickTeamName2}>Change Team Name 2</button>
+              {teamNames2}
+             {/*TeamSheet shows points next to each player on what they have scored */}
+              <button onClick={this.props.closePopup}>Close</button>
+            </label>
+          </div>
+
+          {/* Team 1 score and penalty buttons */}
           <div className="Buttons">
             <div className="team1Buttons">
               <button onClick={this.try1}>Try</button>
@@ -79,6 +139,8 @@ class App extends Component {
               <button onClick={this.dropGoal1}>Drop Goal</button>
               <button onClick={this.penalty1}>Penalty</button>
             </div>
+
+            {/* Team 2 score and penalty buttons */}
             <div className="team2Buttons">
               <button onClick={this.try2}>Try</button>
               <button onClick={this.conversion2}>Conversion</button>
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,56 +1,96 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const ms = require('pretty-ms')
+
 class Timer extends Component {
-    constructor(props){
-      super(props)
-      this.state = {
-        time: 0,
-        isOn: false,
-        start: 0
-      }
- 
-    }
-    startTimer = () => {
+  state = {
+    timerOn: false,
+    timerStart: 0,
+    timerTime: 0
+  };
+
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
+    });
+    this.timer = setInterval(() => {
       this.setState({
-        isOn: true,
-        time: this.state.time,
-        start: Date.now() - this.state.time
-      })
-      this.timer = setInterval(() => this.setState({
-        time: Date.now() - this.state.start
-      }), 1);
+        timerTime: Date.now() - this.state.timerStart
+      });
+    }, 10);
+    // clearInterval = () => {
+      if(this.state.timerTime >= 80){
+          this.stopTimer()
+      }
+    // }
+  };
+
+  stopTimer = () => {
+    this.setState({ timerOn: false });
+    clearInterval(this.timer);
+  };
+  resetTimer = () => {
+    this.setState({
+      timerStart: 0,
+      timerTime: 0
+    });
+  };
+
+  adjustTimer = input => {
+    const { timerTime, timerOn } = this.state;
+    const max = 216000000;
+    if (!timerOn) {
+      if (input === "incHours" && timerTime + 3600000 < max) {
+        this.setState({ timerTime: timerTime + 3600000 });
+      } else if (input === "decHours" && timerTime - 3600000 >= 0) {
+        this.setState({ timerTime: timerTime - 3600000 });
+      } else if (input === "incMinutes" && timerTime + 60000 < max) {
+        this.setState({ timerTime: timerTime + 60000 });
+      } else if (input === "decMinutes" && timerTime - 60000 >= 0) {
+        this.setState({ timerTime: timerTime - 60000 });
+      } else if (input === "incSeconds" && timerTime + 1000 < max) {
+        this.setState({ timerTime: timerTime + 1000 });
+      } else if (input === "decSeconds" && timerTime - 1000 >= 0) {
+        this.setState({ timerTime: timerTime - 1000 });
+      }
     }
-    stopTimer = () => {
-      this.setState({isOn: false})
-      clearInterval(this.timer)
-    }
-    resetTimer = () => {
-      this.setState({time: 0, isOn: false})
-    }
-    render() {
-      let start = (this.state.time === 0) ?
-        <button onClick={this.startTimer}>start</button> :
-        null
-      let stop = (this.state.time === 0 || !this.state.isOn) ?
-        null :
-        <button onClick={this.stopTimer}>stop</button>
-      let resume = (this.state.time === 0 || this.state.isOn) ?
-        null :
-        <button onClick={this.startTimer}>resume</button>
-      let reset = (this.state.time === 0 || this.state.isOn) ?
-        null :
-        <button onClick={this.resetTimer}>reset</button>
-      return(
-        <div>
-          <h3>{ms(this.state.time)}</h3>
+  };
+  render() {
+    const { timerTime } = this.state;
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+
+    let start = (this.state.timerStart === 0) ?
+      <button onClick={this.startTimer}>start</button> :
+      null
+
+    let stop = (this.state.timeTimer === 0 || !this.state.timerOn) ?
+      null :
+      <button onClick={this.stopTimer}>stop</button>
+
+    let resume = (this.state.timeTimer === 0 || this.state.timerOn) ?
+      null :
+      <button onClick={this.startTimer}>resume</button>
+
+    let reset = (this.state.timeTimer === 0 || this.state.timerOn) ?
+      null :
+      <button onClick={this.resetTimer}>reset</button>
+
+    return (
+      <div className="Stopwatch">
+        <div className="Stopwatch-display">
+          {minutes} : {seconds} : {centiseconds}
+        </div>
+        <div className="TimerButtons">
           {start}
-          {resume}
           {stop}
+          {resume}
           {reset}
         </div>
-      )
-    }
+      </div>
+    );
   }
-
+}
 export default Timer;
